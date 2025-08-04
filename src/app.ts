6bb0a968134b8248615c16ae85d16edb5f18a16f
Yusuf <yusufali.5094@gmail.com>
2025-08-04 23:09:50 +0530

    setup file based configuration

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.json({ message: "Hello from the catalog service!" });
 });
 
 app.use(globalErrorHandler);
