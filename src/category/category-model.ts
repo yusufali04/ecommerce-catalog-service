0ac699024e88435398480d0667fca0f96d34db32
Yusuf <yusufali.5094@gmail.com>
2025-08-05 22:47:35 +0530

    add category model

---
+++ src/category/category-model.ts
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+interface PriceConfiguration {
+    [key: string]: {
+        priceType: "base" | "additional";
+        availableOptions: string[];
+    };
+}
+
+interface Attribute {
+    name: string;
+    widgetType: "switch" | "radio";
+    defaultValue: string;
+    availableOptions: string[];
+}
+
+export interface Category {
+    name: string;
+    priceConfiguration: PriceConfiguration;
+    attributes: Attribute[];
+}
+
+const priceConfigurationSchema = new mongoose.Schema<PriceConfiguration>({
+    priceType: {
+        type: String,
+        enum: ["base", "additional"],
+        required: true,
+    },
+    availableOptions: {
+        type: [String],
+        required: true,
+    },
+});
+
+const attributeSchema = new mongoose.Schema<Attribute>({
+    name: {
+        type: String,
+        required: true,
+    },
+    widgetType: {
+        type: String,
+        enum: ["switch", "radio"],
+        required: true,
+    },
+    defaultValue: {
+        type: mongoose.Schema.Types.Mixed,
+        required: true,
+    },
+    availableOptions: {
+        type: [String],
+        required: true,
+    },
+});
+
+const categorySchema = new mongoose.Schema<Category>({
+    name: {
+        type: String,
+        required: true,
+    },
+    priceConfiguration: {
+        type: Map,
+        of: priceConfigurationSchema,
+        required: true,
+    },
+    attributes: {
+        type: [attributeSchema],
+    },
+});
+const CategoryModel = mongoose.model<Category>("Category", categorySchema);
+
+export default CategoryModel;
